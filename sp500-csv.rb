9f3281a69d407dc113bfe5167a5be266192130ef
trungng92 <trung_nguyen@ultimatesoftware.com>
2018-05-17 00:29:06 -0400

    Remove first row. Read numeric inputs as floats.

--- sp500-csv.rb
+++ sp500-csv.rb
@@ -9,6 +9,9 @@ $CSV_FILENAME = 'sp500-monthly.csv'
 $SP500_MONTHLY = []
 
 # First row is the header, so we ignore that
-CSV.foreach($CSV_FILENAME, {:headers=>:first_row}) do |row|
-  $SP500_MONTHLY.push DataPoint.new(*row)
+CSV.foreach($CSV_FILENAME) do |row|
+  $SP500_MONTHLY.push DataPoint.new(row[0], row[1].to_f, row[2].to_f, row[3].to_f, row[4].to_f, row[5].to_f, row[6].to_f)
 end
+
+# Remove the first element, which was the headers row
+$SP500_MONTHLY.shift
