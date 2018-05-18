a17463fe77b48f0f814fbaf56c7ced92b0df7261
trungng92 <trung_nguyen@ultimatesoftware.com>
2018-05-17 23:38:17 -0400

    Change strategy to be less hardcoded. We can now pass in the data points and the strategies, and each strategy's results will be returned.

--- frontload-vs-average.rb
+++ frontload-vs-average.rb
@@ -5,7 +5,7 @@ require_relative 'sp500-csv'
 
 # TODO: Allow start date and end date to be read in from args
 # For now just take the full range
-$START_INDEX = 0;
+$START_INDEX = 0
 $END_INDEX = $SP500_MONTHLY.size
 
 $SP500_MONTHLY_SLICE=$SP500_MONTHLY[$START_INDEX..$END_INDEX]
@@ -21,43 +21,47 @@ strategies = {
   :strategy_average => [Rational(1, 12)] * 12,
 }
 
-# Keeps track of how well each strategy is performing each month
-strategy_results = {
-    :strategy_frontload => [0],
-    :strategy_first_three_months => [0],
-    :strategy_first_six_months => [0],
-    :strategy_average => [0],
-}
-
-# Validate strategies are good
+# Validate strategies
 strategies.each do |strategy_name, strategy_array|
   raise "strategy #{strategy_name} does not add up to 1" unless strategy_array.reduce(:+) == 1
   raise "strategy #{strategy_name} does not add have 12 elements" unless strategy_array.size == $MONTHS_PER_YEAR
 end
 
-current_price = $SP500_MONTHLY_SLICE[0][:close]
-current_month = 0
-$SP500_MONTHLY_SLICE.each do |data_point|
-  puts "\n\nNew Month: #{data_point[:date]}"
-  puts "data point is #{data_point}"
-  new_price = data_point[:close]
-  puts "new price is #{new_price}"
-  percent_change = new_price / current_price
-  puts "percent change is #{percent_change}"
+# Calculates the gain of each strategy over the given data points
+def calculate_strategy(data_points, strategies)
+  # Make sure that strategies_copy is _at least_ as long as data_points and iterate through all items at the same time
+  strategies_size_increase = (data_points.size.to_f / $MONTHS_PER_YEAR).ceil
+  strategies_copy = strategies.clone
+  strategies_copy.each { |k, v| strategies_copy[k] = v * strategies_size_increase }
 
-  strategies.each do |strategy_name, strategy_array|
-    current_gains = strategy_results[strategy_name].last
-    puts "#{strategy_name} current gains is #{current_gains}"
-    strategy_current_month_gains = strategy_array[current_month]
-    puts "#{strategy_name} current month gains is #{strategy_current_month_gains}"
-    strategy_results[strategy_name].push((current_gains + strategy_current_month_gains) * percent_change)
-    puts "#{strategy_name} current results are #{strategy_results[strategy_name]}"
+  # Keeps track of how well each strategy is performing each month
+  strategy_results = {
+      :strategy_frontload => [0],
+      :strategy_first_three_months => [0],
+      :strategy_first_six_months => [0],
+      :strategy_average => [0],
+  }
+
+  data_points.each_index do |i|
+    date = data_points[i][:date]
+    puts "Date: #{date}"
+    old_close = i == 0 ? data_points[0][:close] : data_points[i-1][:close]
+    close = data_points[i][:close]
+    change_ratio = close / old_close
+    puts "Close: #{old_close} -> #{close}; Ratio #{change_ratio}\n\n"
+
+    strategies_copy.each do |strat_name, strat_array|
+      previous_gains = i == 0 ? 0 : strategy_results[strat_name][i - 1]
+      puts "#{strat_name} Previous gains: #{previous_gains}"
+      puts "#{strat_name} New gains: #{strat_array[i]}"
+      strategy_results[strat_name][i] = (previous_gains + strat_array[i]) * change_ratio
+      puts "#{strat_name} Current gains #{strategy_results[strat_name][i]}\n\n"
+    end
+
+    puts "\n\n"
   end
 
-  current_price = new_price
-  current_month = (current_month + 1) % $MONTHS_PER_YEAR
+  return strategy_results
 end
 
-strategy_results.each do |strategy_name, strategy_results|
-  puts "#{strategy_name} final gains: #{strategy_results.last}"
-end
+calculate_strategy($SP500_MONTHLY_SLICE, strategies)
