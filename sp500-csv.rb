321e2a365e05c0980188252441c2ea20d7305ca5
trungng92 <trung_nguyen@ultimatesoftware.com>
2018-05-19 23:55:13 -0400

    Modify csv lib to be more lib like.

--- sp500-csv.rb
+++ sp500-csv.rb
@@ -4,14 +4,16 @@
 require 'csv'
 
 DataPoint = Struct.new(:date, :open, :high, :low, :close, :adj_close, :volume)
-$CSV_FILENAME = 'sp500-monthly.csv'
 
-$SP500_MONTHLY = []
+def read_csv filename
+  data = []
+  CSV.foreach(filename) do |row|
+    data.push DataPoint.new(row[0], row[1].to_f, row[2].to_f, row[3].to_f, row[4].to_f, row[5].to_f, row[6].to_f)
+  end
 
-# First row is the header, so we ignore that
-CSV.foreach($CSV_FILENAME) do |row|
-  $SP500_MONTHLY.push DataPoint.new(row[0], row[1].to_f, row[2].to_f, row[3].to_f, row[4].to_f, row[5].to_f, row[6].to_f)
+  # Remove the first element, which was the headers row
+  data.shift
+
+  return data
 end
 
-# Remove the first element, which was the headers row
-$SP500_MONTHLY.shift
